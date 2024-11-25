abb5bf100fb1a062fa0de3533d6325dd2b1364ac
Oscar Mercado <omercador@unicartagena.edu.co>
2024-11-24 20:37:48 -0500

    agregando talleres

---
+++ Taller_11/Clases Abstractas/src/Ejercicio3/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ejercicio3;
+
+/**
+ *
+ * @author Oscar Mercado
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Intento de instanciar la clase abstracta
+        Animal animal = new Animal();  // Error de compilación
+        
+    }
+    
+}
